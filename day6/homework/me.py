90b8d3d2ba28a929deab0f04a2418a0f63c7fa10
Amiko_A <ggpleier33@gmail.com>
2024-06-06 15:18:37 +0400

    seventh commit

---
+++ day6/homework/me.py
@@ -0,0 +1,10 @@
+#თქვენს თავზე შემოიტანეთ ინფორმაცია: სახელი, გვარი, ასაკი, ქვეყანა, საყვარელი მანქანა, საყვარელი ფერი და ფავორიტი რიცხვი, შემოტანილი მნიშვნელობებით ააწყვეთ ერთი დიდი წინადადება.
+my_name="Amiko"
+my_last_name="Amiridze"
+my_age=14
+my_country="Georgia"
+my_favorite_car="nissan silvia s15"
+my_favorite_color="midnight purple"
+my_favorite_number=3
+print("hello my name is",my_name,my_last_name,"i am",my_age,"old,i am from",my_country,"my favorite car is",my_favorite_car,"my favorite color is",
+      my_favorite_color,"and my favororite number is",my_favorite_number,"byeee")
