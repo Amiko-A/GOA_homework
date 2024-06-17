b20715170673e66246af9cb6986ecc7ad9c86d78
Amiko_A <ggpleier33@gmail.com>
2024-06-17 18:48:50 +0400

    tenth commit

---
+++ day10/homework/lesson 2 11.py
@@ -0,0 +1,11 @@
+#დაწერეთ პროგრამა, რომელიც იღებს ორი ადამიანის ასაკს და განსაზღვრავს ვინ არის უფროსი
+Mother= int(input("Pleace enter Your mothers age : "))
+
+Father= int(input("Pleace enter Your fathers age : "))
+
+if Father <= Mother :
+    print ("Mother Is Older")
+
+else :
+    Mother <= Father 
+    print("Mother Is Younger")
