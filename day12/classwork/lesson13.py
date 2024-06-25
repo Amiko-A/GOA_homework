6f87aa58abc42433f3ce2d6826b3282e7d67079e
Amiko_A <ggpleier33@gmail.com>
2024-06-25 22:13:01 +0400

    thirteenth commit

---
+++ day12/classwork/lesson13.py
@@ -0,0 +1,6 @@
+#the program takes the score as an input
+score=input("Enter your score:")
+#add the comparison operations inside the parentheses
+score=120
+print(score>100 )
+print("you have got to next lvl")
---
+++ day12/homework/level13.py
@@ -0,0 +1,31 @@
+#გააკეთეთ 10 მაგალითი ლოგიკურ ოპერატორებზე(True and False  - ამ გზით არა)
+
+heart_reat=70
+print(heart_reat > 65)
+
+water_temperature=23
+print(water_temperature < 36)
+
+population_in_antarctica=5100
+print(population_in_antarctica <= 131234)
+
+jupiter_moons=95
+print(jupiter_moons >= 2)
+
+population_in_georgia=301730
+print(population_in_georgia == 301730)
+
+dogs_on_world=900000
+print(dogs_on_world != 100000000)
+
+lenght_of_equador=24901
+print(lenght_of_equador <= 24901)
+
+oldest_human_in_history=116
+print(oldest_human_in_history >= 116)
+
+my_age=14
+print(my_age < 15)
+
+oldest_programming_language=67
+print(oldest_programming_language > 13)
