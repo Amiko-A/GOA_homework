cdecaa822e4a6da16f7a1ba02e4288ea5e3f5b83
Amiko_A <ggpleier33@gmail.com>
2024-07-01 20:03:39 +0400

    fourthirty commit

---
+++ day14/homework/input1.py
@@ -0,0 +1,11 @@
+#პირველი დავალება მომხმარებელმა უნდა შემოიტანოს რიცხვი 1 - დან 1000 - მდე, თქვენი მიზანია, რომ გამოიცნოთ შემოტანილი რიცხვი, თუ სწორი იქნება დაპრინტეთ 'სწორია!', ხოლო თუ არასწორია, დაპრინტეთ ;არასწორია!'
+
+num1=int(input("Please enter your number throught 1 from 1000:"))
+num2=1
+if num1>num2 :
+  print("I couldn't guess your number")
+  
+else :
+ num1=num2
+ print("Yess i guessed your number")
+
