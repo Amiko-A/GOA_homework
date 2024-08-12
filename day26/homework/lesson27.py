cf6f680143054814e61b488242eecd8134077eed
Amiko_A <ggpleier33@gmail.com>
2024-08-12 20:35:03 +0400

    twentyseven commit

---
+++ day26/homework/lesson27.py
@@ -0,0 +1,8 @@
+#შექმენით სია და შეიტანეთ 10 სხვადასხვა მონაცემთა ტიპის მნიშვნელობა, შემდეგ 5 მონაცემს  შეუცვალეთ   მნიშვნელობა და შემდეგ დაპრინტეთ მთლიანი სია.
+products=["sugar","potato","garlic","butter","bread","soap","shampoo","textbook","poster","forks"]
+products[0]="salt"
+products[1]="carrot"
+products[6]="black bread"
+products[8]="book"
+products[9]="knife"
+print(products)
