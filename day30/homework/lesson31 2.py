382b9439ea2be81db6844c12723b1d43924ba8a7
Amiko_A <ggpleier33@gmail.com>
2024-08-22 23:59:03 +0400

    thirtyone commit

--- day30/homework/lesson31 2.py
+++
@@ -1,4 +0,0 @@
-def my_title(name):
-    print(name.lower())
-
-my_title(input('enter your name: '))
--- day30/homework/lesson31.py
+++
@@ -1,4 +0,0 @@
-def my_title(name):
-    print(name.upper())
-
-my_title(input('enter your name: '))
