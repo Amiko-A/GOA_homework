af67b15a9343d35995ac7abad96e0ee9da194551
Amiko_A <ggpleier33@gmail.com>
2024-09-12 18:58:32 +0400

    twentyeight commit

---
+++ day36/classwork/lesson37 2.py
@@ -0,0 +1,11 @@
+nums=[10, 20, 30, 40, 50, 60, 70, 80, 90]
+nums .append(100)
+nums .insert(0,5)
+nums .remove(30)
+nums .sort()
+nums .reverse()
+numss=nums.index(50)
+num=nums.count(20)
+print(nums)
+print(num)
+print(numss)
---
+++ day36/classwork/lesson37.py
@@ -0,0 +1,9 @@
+fruites=["apple","banana","cherry","date","elderberry"]
+print(fruites)
+print(fruites[0])
+print(fruites[4])
+fruites += ["fig"]
+fruites .remove ("banana")
+fruites[1]="blueberry"
+print(fruites)
+print(len(fruites))
