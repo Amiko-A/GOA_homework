a8d40e64f95b212b8cf4144dcc84dbbdf7d00538
Amiko_A <ggpleier33@gmail.com>
2024-09-26 20:58:08 +0400

    fourtytwo commit

---
+++ day41/lesson42 2.py
@@ -0,0 +1,6 @@
+# Task 3: Joining Strings
+#Create a list named words that contains the following strings: "apple", "banana", "cherry".
+#Use the join() function to combine these words into a single string, separated by a comma and a space ", ". Store the result in a variable named fruit_string and print it.
+fruit={"apple","banana","cherry"}
+fruit_string = ", ".join(fruit)
+print(fruit_string)
---
+++ day41/lesson42 4.py
@@ -0,0 +1,6 @@
+# Task 5: Replacing Substrings
+#Create a string named quote with the following content: "To be or not to be, that is the question."
+#Use the replace() function to replace the word "be" with "code". Store the result in a variable named modified_quote and print it.
+quote=("To be or not to be, that is the question")
+modified_quote=quote.replace("be" , "code")
+print(modified_quote)
