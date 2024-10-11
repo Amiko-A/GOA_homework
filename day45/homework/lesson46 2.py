7fdbfcc585a9a51bdea3bb850ed0266353c1c3e9
Amiko_A <ggpleier33@gmail.com>
2024-10-11 16:20:02 +0400

    fourtysix commit

---
+++ day45/homework/lesson46 2.py
@@ -0,0 +1,16 @@
+def age(age):
+    if age<0:
+        return "you aren't even born and you have a phone ? crayzyyyy"
+    elif age<10:
+        return "still a kid"
+    elif age<18:
+        return "ok your getting somewhere"
+    elif age<30:
+        return "you have family right?"
+    elif age<60:
+        return "your retired now"
+    elif age>60 :
+        return "Your old pal"
+    else :
+        age>100 
+        return"yeah no"
---
+++ day45/homework/lesson46.py
@@ -0,0 +1,8 @@
+def num(num):
+    if num > 0:
+        return "number is possitve"
+    elif num < 0:
+        return "number is negatuve"
+    else :
+        num==0
+        return "number is zero"
