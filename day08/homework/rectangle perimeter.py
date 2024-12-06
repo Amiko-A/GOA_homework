a81d2eca1db33300680716867e3faa210c1f1216
Amiko_A <ggpleier33@gmail.com>
2024-12-06 16:14:44 +0400

    sixty commit

--- day08/homework/rectangle perimeter.py
+++
@@ -1,6 +0,0 @@
-#დაწერეთ პითონის პროგრამა, რომელიც ითვლის მართკუთხედის პერიმეტრს. პროგრამამ უნდა სთხოვოს მომხმარებელს მართკუთხედის სიგანე და სიმაღლე.
-print("Hello , what is the width of your rectangle?")
-width=int(input("you:"))
-print("And what is the lenght of your rectangle?")
-lenght=int(input("you:"))
-print((width+lenght)*2)
--- day08/homework/rectangle.py
+++
@@ -1,6 +0,0 @@
-#დაწერეთ პითონის პროგრამა, რომელიც ითვლის მართკუთხედის ფართობს. პროგრამამ უნდა სთხოვოს მომხმარებელს მართკუთხედის სიგანე და სიმაღლე.
-print("Hello , what is the lenght of your rectangle?")
-lenght=int(input("you:"))
-print("And how tall is your rectangle?")
-size=int(input("you:"))
-print(lenght*size)
